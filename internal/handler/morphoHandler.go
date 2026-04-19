7a2e56df1f859a6027e99f105562ed4c2718e8fc
Billy19191 <bill.ratchanon@gmail.com>
2026-04-20 02:43:09 +0700

    🐛 fix: wrong logic causing entire morpho api endpoint down

--- internal/handler/morphoHandler.go
+++ internal/handler/morphoHandler.go
@@ -26,25 +26,26 @@ func (h *morphoHandler) GetCurrentVaultPosition(context *gin.Context) {
 		context.JSON(http.StatusBadRequest, gin.H{
 			"error": "walletAddress is required",
 		})
-
-		chainIDInt, err := strconv.Atoi(chainID)
-		if err != nil {
-			context.JSON(http.StatusBadRequest, gin.H{
-				"error": "Invalid chainID",
-			})
-			return
-		}
-
-		result, err := h.service.GetVaultPositionByWallet(walletAddress, chainIDInt)
-		if err != nil {
-			context.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		context.JSON(http.StatusOK, gin.H{
-			"data": result.Data.Vault,
-		})
+		return
 	}
+
+	chainIDInt, err := strconv.Atoi(chainID)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid chainID",
+		})
+		return
+	}
+
+	result, err := h.service.GetVaultPositionByWallet(walletAddress, chainIDInt)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"data": result.Data.Vault,
+	})
 }
